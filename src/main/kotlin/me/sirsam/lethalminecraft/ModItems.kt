e59b5de79ad47dc0df77071f67dfdaad8cfe05ba
SirMo_ <fischer.moritz09@gmail.com>
2025-10-02 15:14:50 +0200

    ModItems

--- src/main/kotlin/me/sirsam/lethalminecraft/ModItems.kt
+++ src/main/kotlin/me/sirsam/lethalminecraft/ModItems.kt
@@ -5,13 +5,17 @@ import net.minecraft.registry.Registries
 import net.minecraft.registry.Registry
 import net.minecraft.registry.RegistryKey
 import net.minecraft.registry.RegistryKeys
+import net.minecraft.util.Identifier
 
 
 class ModItems {
-    private fun register(name: String, itemFactory: Item, settings: Item.Settings) {
-        val itemKey = RegistryKey = RegistryKey.of(RegistryKeys.ITEM, Identifier )
-        Registry.register(Registries.ITEM, item)
+    private fun register(name: String, itemFactory: (Item.Settings) -> Item, settings: Item.Settings): Item {
+        val id = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(LethalMinecraft.MOD_ID))
+        val item = itemFactory(settings)
+        return Registry.register(Registries.ITEM, id, item)
     }
-
-
+    init {
+        val flashlight = register("flashlight", ::Item, Item.Settings())
+        val proFlashLight = register("pro_flashlight", ::Item, Item.Settings())
+    }
 }
