76f7047f922133e15d5757a184dea100dbb72ffa
SirSamX <115340828+SirSamX@users.noreply.github.com>
2025-10-02 13:53:39 +0200

    Init

---
+++ src/client/kotlin/me/sirsam/lethalminecraft/client/LethalminecraftClient.kt
@@ -0,0 +1,9 @@
+package me.sirsam.lethalminecraft.client
+
+import net.fabricmc.api.ClientModInitializer
+
+class LethalminecraftClient : ClientModInitializer {
+
+    override fun onInitializeClient() {
+    }
+}
---
+++ src/main/kotlin/me/sirsam/lethalminecraft/Lethalminecraft.kt
@@ -0,0 +1,9 @@
+package me.sirsam.lethalminecraft
+
+import net.fabricmc.api.ModInitializer
+
+class Lethalminecraft : ModInitializer {
+
+    override fun onInitialize() {
+    }
+}
